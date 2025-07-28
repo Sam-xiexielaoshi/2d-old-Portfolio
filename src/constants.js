b21ee123949506e12725e0bf18193aae2527a923
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-28 20:45:14 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Lua and C#.
+    Here is my <a href="https://github.com/Sam-xiexielaoshi" target="_blank">Github</a>!`,
+  "cs-degree": `Yep, right here's where the ol' computer science degree goes. Four years, a mountain of bugs, sleepless nights, and way too much instant noodles... all for this moment. Feels kinda poetic, doesn’t it?.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube, NetFlix and many more. 
+  I also watch different types of games while analyzing the methods and procedures they used to create it on YouTube. Go sub to <a href="https://www.youtube.com/@ItsManishMax" target="_blank">my friend's channel</a>! (If you like the content)
+  You'll learn .... something I guess!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@RealCivilEngineerGaming" target="_blank">Real Civil Engineer</a>, <a href="https://www.youtube.com/@CodeMonkeyUnity" target="_blank">Code Monkey</a>,
+  <a href="https://www.youtube.com/@theRadBrad" target="_blank">theRadBrad</a> (sometimes) and <a href="https://www.youtube.com/@markiplier" target="_blank">Markiplier</a>!`,
+  bed: `This here's where I lay my head — my humble spawn point. Funny thing, the best redstone contraptions and build ideas come creepin' in when I'm tucked under the wool. When inspiration strikes, I've gotta scribble it down quick, or I'll be tossin' and turnin' like a zombie in daylight. Can't sleep with ideas cloggin' the hopper, y'know?`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Sam-xiexielaoshi/resume/blob/main/Sam-androidDev.pdf" target="_blank">Check it out?</a>
+  Contact me at samagrasingh20032003@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Spanish (I also speak a little bit of Spanish btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Sam's portfolio, just close the tab.`,
+};
